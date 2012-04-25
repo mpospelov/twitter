68af1502842fac850e0a03f5a6a01b80ced3044a
Michail A. Pospelov <mixan946@yandex.ru>
2012-04-25 23:25:48 +0400

    my com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,7 @@ TESTProjects::Application.configure do
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
-
+  config.assets.manifest = Rails.root.join("public/assets")
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -46,8 +45,7 @@ TESTProjects::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( application.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
