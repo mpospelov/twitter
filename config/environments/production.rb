f3bc91fe8adf414b625217025aa62b52efb1f570
Michail A. Pospelov <mixan946@yandex.ru>
2012-04-25 22:44:37 +0400

    my com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ TESTProjects::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.path = Rails.root.join('public', 'assets')
+  config.assets.paths << Rails.root.join('public', 'assets')
   # Generate digests for assets URLs
   config.assets.digest = true
 
