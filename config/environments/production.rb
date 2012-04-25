334e3c4b4891af50f9d442efdaf75b668bb80b43
Michail A. Pospelov <mixan946@yandex.ru>
2012-04-25 23:37:11 +0400

    my com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ TESTProjects::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.paths << Rails.root.join('public', 'assets')
   # Generate digests for assets URLs
   config.assets.digest = true
